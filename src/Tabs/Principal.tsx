99e5127daf7f4c73c89ff44feb1445649e7ef3c0
Lucas <gomes.lucasj@gmail.com>
2024-04-23 22:29:24 -0300

    aula 5 - criar tela principal

--- src/Tabs/Principal.tsx
+++ src/Tabs/Principal.tsx
@@ -1,9 +1,46 @@
-import { VStack, Text } from "native-base";
+import { VStack, Image, Box, ScrollView, Text, Divider } from "native-base";
+import Logo from '../assets/Logo.png';
 
-export default function Principal() {
-    return (
-        <VStack>
-            <Text>Principal</Text>
+import { depoimentos } from "../utils/mock";
+import { EntradaTexto } from "../components/EntradaTexto";
+import { Title } from "../components/Title";
+import { Botao } from "../components/Botao";
+
+
+export default function Principal(){
+
+  return (
+    <ScrollView flex={1} bgColor="white">
+      <VStack flex={1} alignItems="flex-start" justifyContent="flex-start" p={5}>
+        <Image source={Logo} alt="Logo" mt={10} />
+        <Title color="blue.500">Boas-vindas!</Title>
+
+        <Box w="100%" borderRadius="lg" p={3} mt={10} shadow="1" borderRightRadius="md">
+          <EntradaTexto
+            placeholder="Digite a especialidade"
+          />
+          <EntradaTexto
+            placeholder="Digite sua localização"
+          />
+          <Botao mt={3} mb={3}>
+            Buscar
+          </Botao>
+        </Box>
+
+        <Title color="blue.800" alignSelf="center">Depoimentos</Title>
+        <VStack space={3} divider={<Divider />} w="100%">
+          {
+            depoimentos.map(depoimento => (
+              <Box key={depoimento.id} w="100%" borderRadius="lg" p={3}>
+                <Text color="gray.300" fontSize="md" textAlign="justify">
+                  {depoimento.text}
+                </Text>
+                <Text color="gray.500" fontSize="lg" fontWeight="bold" alignSelf="center" mt="2">{depoimento.titulo}</Text>
+              </Box>
+            ))
+          }
         </VStack>
-    )
+      </VStack>
+    </ScrollView>
+  );
 }
